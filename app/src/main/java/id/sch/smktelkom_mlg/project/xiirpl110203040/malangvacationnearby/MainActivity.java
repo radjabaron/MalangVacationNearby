1e618b9ef4594a6ca379c4bed2407143a1ad2d70
Widyarso Joko Purnomo <widyjp10@gmail.com>
2016-10-21 22:12:48 +0700

    add maps activity

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl110203040/malangvacationnearby/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl110203040/malangvacationnearby/MainActivity.java
@@ -1,13 +1,33 @@
 package id.sch.smktelkom_mlg.project.xiirpl110203040.malangvacationnearby;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng moklet = new LatLng(-7.977205, 112.658870);
+        mMap.addMarker(new MarkerOptions().position(moklet).title("Marker in Moklet"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(moklet));
     }
 }
