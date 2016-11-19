40d3b80034393251a4e990336984465b46fa47ab
Baron Jahzyradja <baron_jahzyradja_23rpl@student.smktelkom-mlg.sch.id>
2016-11-19 23:30:54 +0700

    Intent GoogleMaps

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl110203040/malangvacationnearby/DetailActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl110203040/malangvacationnearby/DetailActivity.java
@@ -1,11 +1,14 @@
 package id.sch.smktelkom_mlg.project.xiirpl110203040.malangvacationnearby;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
@@ -24,6 +27,14 @@ public class DetailActivity extends AppCompatActivity {
     TextView tvJam;
     TextView tvDesc;
     Firebase ref;
+
+    Button btnOpenMaps;
+    String googleMap = "com.google.android.apps.maps";
+    Uri gmmIntentUri;
+    Intent mapIntent;
+    String masjid_agung_demak = "-6.894649906672214,110.63718136399984";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,11 +69,31 @@ public class DetailActivity extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.btnCallTaxi).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.btnOpenTaxi).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(DetailActivity.this, TaxiActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.btnOpenMaps).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gmmIntentUri = Uri.parse("google.navigation:q=" + masjid_agung_demak);
+
+                // Buat Uri dari intent gmmIntentUri. Set action => ACTION_VIEW
+                mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+
+                // Set package Google Maps untuk tujuan aplikasi yang di Intent yaitu google maps
+                mapIntent.setPackage(googleMap);
+
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                } else {
+                    Toast.makeText(DetailActivity.this, "Google Maps Belum Terinstal. Install Terlebih dahulu.",
+                            Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
